43c4be169b751e5faabfed0a2559d1cf291887ab
Fardeen-1204 <157814755+Fardeen-1204@users.noreply.github.com>
2025-03-23 16:00:52 +0530

    Delete EmpDetails directory

--- EmpDetails/Emp_Info/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from Emp_Info.models import EmpDetails
-
-# Register your models here.
-admin.site.register(EmpDetails)
--- EmpDetails/Emp_Info/templatetags/custom_filters.py
+++
@@ -1,8 +0,0 @@
-from django import template
-register=template.Library()
-
-@register.simple_tag
-def cut_string(val,cut_val):
-    return val[:cut_val]
-
-register.filter('cut_string',cut_string)
